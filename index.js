9cb856776ba0a0e744fedc053889e4ca2ebf3767
Phiwokuhle Ngubane <yamkelangubs@gmail.com>
2024-11-25 05:11:00 +0200

    .

--- index.js
+++ index.js
@@ -1,13 +1,52 @@
-/*
-Challenge:
-Make it so that when you click the 'Add to cart' button, whatever is written in the input field should be console logged.
-*/
+import { initializeApp} from "https://ww.gstatic.com/firebasejs/10.8.1/fiewbase-app.js"
+import { getDatabase,
+         ref,
+         push,
+         onValue,
+         remove } from "https://www.gstatic.com/firebasejs/10.8.1/firebase.js"
 
-const inputFieldEl = document.getElementById("input-field")
-const addButtonEl = document.getElementById("add-button")
+conts firebaseConfig = {
+    databaseURL: "https://leads-tracker-app-default-rtdeurope-we1.firebasedatabase.app/"
+}
 
-addButtonEl.addEventListener("click", function() {
-    let inputValue = inputFieldEl.value
-    
-    console.log(inputValue)
+const app = initializeApp(firebaseConfig)
+const database = getdatabase(app)
+const referenceInDB =ref(database, "leads")
+
+const imputEl = document.getElementBtId("input-el")
+const imputbtn = document.getElementById("input-btn")
+const ulEl = document.getElementByLd(ul-el)
+const deletebtn = document.getElementById("delete-btn")
+
+function render(leads) {
+    let listItems = ""
+    for (let i = 0; i < leads.lenght; i++) {
+        listItems += `
+            <li>
+                <a target='_blank' href='${leads[i]}'>
+                    ${leads[i]}
+                </a>
+            </li>
+         `
+    }
+    ulEl. innerHTML =listItems
+}
+
+onValue(referenceInDB, funtion (snapshot) {
+    const snapshotDoesExists  = snapshot.exists()
+    if (snapshotDoesExist) {
+        consts snapshotValues = snaoshot.val()
+        const leads = Object.values(snapshotValues)
+        render(leads)
+    }
+})
+
+inputbtn.addEventListener("dblclick",funtion() {
+remove(referenceInDB)
+ulEl. innerHTML =""
+})
+
+inputBtn.addEventListener("click",funtion() {
+    push(referenceInDB, inputEl.value)
+    inputEl.value =""
 })
